c962dc1edcce0412cc0652c9211d6db8ad4b3274
Noor Fatima <129104494+NoorFatima-developer@users.noreply.github.com>
2024-09-20 14:49:55 +0500

    Fade Out Added

---
+++ 10.Fade Out/app.js
@@ -0,0 +1,13 @@
+const arrow = document.getElementById('arrow');
+
+window.addEventListener("scroll", () => {
+    let position = window.scrollY;
+    // console.log(position);
+
+    if (position <=5) {
+        arrow.classList.toggle("fade-in");
+        arrow.classList.toggle("fade-out");
+    }
+    
+})
+
