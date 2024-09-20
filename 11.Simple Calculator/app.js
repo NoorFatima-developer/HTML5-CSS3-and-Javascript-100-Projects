f2305a840a39491ca152e092d15fa467dcc86386
Noor Fatima <129104494+NoorFatima-developer@users.noreply.github.com>
2024-09-20 18:28:47 +0500

    Simple Calculator Added

---
+++ 11.Simple Calculator/app.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+
+    let num1 = document.querySelector('.num1').value;
+    let num2 = document.querySelector('.num2').value;
+    let result = document.querySelector('.result');
+    let operator = document.getElementById("selectOp").value;
+
+    switch(operator) {
+        case "plus":
+            result.innerHTML = Number(num1) + Number(num2);
+            break;
+
+        case "min":
+            result.innerHTML = Number(num1) - Number(num2);
+            break;
+
+        case "div":
+            result.innerHTML = Number(num1) / Number(num2);
+            break
+
+        case "multi": 
+        result.innerHTML = Number(num1) * Number(num2);
+
+    } 
+
+});
+
+
